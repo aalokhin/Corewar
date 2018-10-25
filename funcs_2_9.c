2c6b266d0602cd85d57aa3a5f3f93939b2e1618a
Viktoriia LIKHOTKINA <viktoria.likhotkina@gmail.com>
2018-10-25 16:32:51 +0300

    Divide funcs into to part

---
+++ funcs_2_9.c
@@ -0,0 +1,42 @@
+#include "corewar.h"
+#include <stdio.h>
+
+void load(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
+{
+	ft_printf("%s\n", "test");
+}
+
+void store(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
+{
+	ft_printf("%s\n", "test");
+}
+
+void addition(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
+{
+	ft_printf("%s\n", "test");
+}
+
+void substraction(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
+{
+	ft_printf("%s\n", "test");
+}
+
+void bit_and(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
+{
+	ft_printf("%s\n", "test");
+}
+
+void bit_or(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
+{
+	ft_printf("%s\n", "test");
+}
+
+void bit_xor(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
+{
+	ft_printf("%s\n", "test");
+}
+
+void zjmp(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
+{
+	ft_printf("%s\n", "test");
+}
--- l_funcs.c
+++ l_funcs.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 
-
 void live(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
 {
 	int i;
@@ -32,45 +31,6 @@ void live(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
 	}
 }
 
-void load(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
-{
-	ft_printf("%s\n", "test");
-}
-
-void store(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
-{
-	ft_printf("%s\n", "test");
-}
-
-void addition(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
-{
-	ft_printf("%s\n", "test");
-}
-
-void substraction(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
-{
-	ft_printf("%s\n", "test");
-}
-
-void bit_and(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
-{
-	ft_printf("%s\n", "test");
-}
-
-void bit_or(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
-{
-	ft_printf("%s\n", "test");
-}
-
-void bit_xor(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
-{
-	ft_printf("%s\n", "test");
-}
-
-void zjmp(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
-{
-	ft_printf("%s\n", "test");
-}
 
 void load_ind(t_proc *head_proc, int cur_proc, t_cycle *main_cycle)
 {
