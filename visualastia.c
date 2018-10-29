7a019b505a75e7769618af27eac365fd784a3c9f
Viktoriia LIKHOTKINA <viktoria.likhotkina@gmail.com>
2018-10-29 14:42:09 +0200

    Add right output

--- visualastia.c
+++ visualastia.c
@@ -14,7 +14,7 @@ void 	map_to_screen(unsigned char *map, int indexes[MEM_SIZE][2])
 	int  yMax, xMax;
 	getmaxyx(stdscr, yMax, xMax);
 
-	WINDOW * win = newwin(70, xMax-80, yMax-80, 5);
+	WINDOW * win = newwin(69, xMax-100, yMax-80, 5); //*newwin(int nlines, int ncols, int begin_y, int begin_x);
 	// box(win, 0, 0);
 
 	// int wborder(WINDOW *win, chtype ls, chtype rs, chtype ts, chtype bs, chtype tl, chtype tr, chtype bl, chtype br);
@@ -28,6 +28,7 @@ void 	map_to_screen(unsigned char *map, int indexes[MEM_SIZE][2])
 	init_pair(4, COLOR_CYAN, COLOR_BLACK);
 	init_pair(5 , COLOR_WHITE, COLOR_BLACK);
 	init_pair(6 , COLOR_RED, COLOR_RED);
+	init_pair(7 , COLOR_CYAN, COLOR_BLACK);
 
 
 
@@ -55,7 +56,7 @@ map[0] = map[0];
 	while (i < MEM_SIZE + 1)
 	{
 		x = 3;
-   		while (x < 192)
+   		while ( x < 194)
     	{
     		if (indexes[i][0] == 1)
     		{
@@ -130,6 +131,68 @@ map[0] = map[0];
 
     	y++;
 	}
+	x = 199;
+	y = 2;
+	char name[] = "name";
+	mvwprintw(win, y, x,  "** PAUSED **");
+
+
+	y += 2;
+	
+	int replace = 0;
+	mvwprintw(win, y, x,  "Cycles/second limit : %d", replace);
+
+	y += 4;
+
+	mvwprintw(win, y, x,  "Cycles: %d", replace);
+	y += 2;
+
+	mvwprintw(win, y, x,  "Processes: %d", replace);
+	y += 2;
+	mvwprintw(win, y, x,  "Player: %d : ", replace, name);
+	x+=2;
+	y++;
+	mvwprintw(win, y, x,  "Last live: %d : ", replace, name);
+	y++;
+
+	mvwprintw(win, y, x,  "Lives in current period : %d ", replace, name);
+	y += 2;
+	x-=2;
+	mvwprintw(win, y, x,  "Live breakdown for current period :");
+
+	y += 2;
+
+wattron(win, COLOR_PAIR(7));
+	mvwprintw(win, y, x,  "[--------------------------------------------------]");
+	wattroff(win, COLOR_PAIR(7));
+
+	y += 2;
+
+	mvwprintw(win, y, x,  "Live breakdown for last period :");
+
+	y += 2;
+
+
+	mvwprintw(win, y, x,  "Live breakdown for last period :");
+
+	wattron(win, COLOR_PAIR(7));
+		mvwprintw(win, y, x,  "[--------------------------------------------------]");
+		wattroff(win, COLOR_PAIR(7));
+
+		y += 2;
+
+		mvwprintw(win, y, x,  "CYCLE_TO_DIE : %d", replace);
+		y += 2;
+
+		mvwprintw(win, y, x,  "NBR_LIVE : %d", replace);
+		y += 2;
+
+		mvwprintw(win, y, x,  "MAX_CHECKS : %d", replace);
+
+	
+	
+
+	
 	wrefresh(win);
 	 getch();
 	endwin();
