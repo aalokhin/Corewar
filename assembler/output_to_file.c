7465b04b93201f5a27a4abb28c4cdb9d32b7cdd1
Anastasiia ALOKHINA <aalokhin@e2r3p7.unit.ua>
2018-11-09 14:45:10 +0200

    output fixed, arg_val not yet fixed

--- assembler/output_to_file.c
+++ assembler/output_to_file.c
@@ -17,7 +17,6 @@ void	fill_corfile_contents(t_binfile *bin)
 	i = 0;
 	while(i < 4)
 	{
-		//printf("writing ....\n");
 		write(bin->fd_file_out, &bin->magic_start[i], 1);
 		i++;
 	}
@@ -34,11 +33,8 @@ void	fill_corfile_contents(t_binfile *bin)
 	write(bin->fd_file_out, "\0\0\0\0", 4);
 
 	i = 0;
-	// printf("BIN_COMMENT:          %s\n",bin->comment );
 	while (i < COMMENT_LENGTH)
 	{
-		// if (i <= 10)
-		// 	printf("===444444=====>%c\n", bin->comment[i]);
 		write(bin->fd_file_out, &bin->comment[i], 1);
 		i++;
 
@@ -54,14 +50,11 @@ void	fill_corfile_contents(t_binfile *bin)
 		while(tmp)
 		{
 			a[0] = tmp->opcode & 0xFF;
-			a[1] = (tmp->opcode >> 8) & 0xFF;
 			
 			write (bin->fd_file_out, &a, 1);
 			if (tmp->has_codage)
 			{
 				a[0] = tmp->codage & 0xFF;
-				a[1] = (tmp->codage >> 8) & 0xFF;
-				i = 0;
 				write (bin->fd_file_out, &a, 1);
 			}
 			i = 0;
@@ -73,18 +66,24 @@ void	fill_corfile_contents(t_binfile *bin)
 				printf("writing arg_value:   %d\n", tmp->args[i][1]);
 
 
-				//write(bin->fd_file_out, &tmp->args[i][0], 1);
-				
-				// if(&tmp->args[i][0])
-				// {
 				if (tmp->args[i][0] == 10)
 				{
 					if (tmp->lbl_size == 4)
 					{
-						a[0] = tmp->args[i][1] & 0x000000FF;
-						a[1] = (tmp->args[i][1] & 0x0000FF00) >> 8;
-						a[2] = (tmp->args[i][1] & 0x00FF0000) >> 16;
-						a[2] = (tmp->args[i][1] & 0xFF000000) >> 24;
+						// a[0] = tmp->args[i][1] & 0x000000FF;
+						// a[1] = (tmp->args[i][1] & 0x0000FF00) >> 8;
+						// a[2] = (tmp->args[i][1] & 0x00FF0000) >> 16;
+						// a[3] = (tmp->args[i][1] & 0xFF000000) >> 24;
+						// z =  0;
+						// while(z < 4)
+						// {
+						// 	write(bin->fd_file_out, &a[z], 1);
+						// 	z++;
+						// }
+						a[3] = tmp->args[i][1] & 0x000000FF;
+						a[2] = (tmp->args[i][1] & 0x0000FF00) >> 8;
+						a[1] = (tmp->args[i][1] & 0x00FF0000) >> 16;
+						a[0] = (tmp->args[i][1] & 0xFF000000) >> 24;
 						z =  0;
 						while(z < 4)
 						{
@@ -94,8 +93,18 @@ void	fill_corfile_contents(t_binfile *bin)
 					}
 					else // == 2
 					{
-						a[0] = tmp->args[i][1] & 0xFF;
-						a[1] = (tmp->args[i][1] >> 8) & 0xFF;
+						// a[0] = tmp->args[i][1] & 0xFF;
+						// a[1] = (tmp->args[i][1] >> 8) & 0xFF;
+						// z =  0;
+						// while(z < 2)
+						// {
+						// 	write(bin->fd_file_out, &a[z], 1);
+						// 	z++;
+						// }
+
+
+						a[1] = tmp->args[i][1] & 0xFF;
+						a[0] = (tmp->args[i][1] >> 8) & 0xFF;
 						z =  0;
 						while(z < 2)
 						{
@@ -107,8 +116,17 @@ void	fill_corfile_contents(t_binfile *bin)
 				}
 				else if (tmp->args[i][0] == 11)
 				{
-					a[0] = tmp->args[i][1] & 0xFF;
-					a[1] = (tmp->args[i][1] >> 8) & 0xFF;
+					// a[0] = tmp->args[i][1] & 0xFF;
+					// a[1] = (tmp->args[i][1] >> 8) & 0xFF;
+					// z =  0;
+					// while(z < 2)
+					// {
+					// 	write(bin->fd_file_out, &a[z], 1);
+					// 	z++;
+					// }
+
+					a[1] = tmp->args[i][1] & 0xFF;
+					a[0] = (tmp->args[i][1] >> 8) & 0xFF;
 					z =  0;
 					while(z < 2)
 					{
@@ -121,12 +139,8 @@ void	fill_corfile_contents(t_binfile *bin)
 					a[0] = tmp->args[i][1] & 0xFF;
 					write(bin->fd_file_out, &a[0], 1);
 				}
-				//write(bin->fd_file_out, &a[0], 1);
 				
 
-
-
-				// }
 				i++;
 			}
 
