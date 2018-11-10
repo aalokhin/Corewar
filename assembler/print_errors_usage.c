eff1ee3e09cbf9beba49ceee5b3a70a7ef7bc1ac
mdynia1 <34481108+mdynia1@users.noreply.github.com>
2018-11-10 22:25:19 +0200

    Add files via upload

--- assembler/print_errors_usage.c
+++ assembler/print_errors_usage.c
@@ -31,4 +31,44 @@ void	ft_print_flag_a(t_binfile *bin)
 	
 }
 
+int			error_message_label(char *label, char *arg)
+{
+	printf("No such label %s while attempting to dereference token [TOKEN][%d:%d] %s {%s}\n",  label, 0, 0, "DIRECT_LABEL", arg);
+	return (0);
+}
 
+int			error_message(char *arg)
+{
+	char 	*e;
+	int 	type;
+
+	type = (ft_strchr(arg ,'r') && !(ft_strchr(arg ,'%'))) ? 1 : ft_strchr(arg ,'%') ? 2 : ft_strchr(arg , SEPARATOR_CHAR) ? 5 : ft_atoi(arg) != 0 ? 4 : 0;
+	if (type == 1)
+		e = ft_strdup("REGISTER");
+	else if (type == 2)
+		e = ft_strdup("DIRECT");
+	else if (type == 4)
+		e = ft_strdup("INDIRECT");
+	else if (type == 5)
+		e = ft_strdup("SEPARATOR");
+	else
+		e = ft_strdup("INSTRUCTION");
+	printf("Syntax error at token [TOKEN][%d:%d] %s {%s}\n", 0, 0, e, arg);
+	//printf("%s\n", w_str);
+
+	return (0);
+}
+
+int 		error_invalid_arg_type(t_t *command, int arg, int size)
+{
+	char 	*type;
+
+	if (size == 1)
+		type = ft_strdup("register");
+	else if (size == 2)
+		type = ft_strdup("direct");
+	else
+		type = ft_strdup("indirect");
+	ft_printf("Invalid parameter %d type %s for instruction %s\n", arg, type, command->name_c);
+	return (0);
+}
