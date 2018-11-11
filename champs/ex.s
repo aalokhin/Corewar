2c1c5442bd8b069b042a08da637c2350f234f34a
Anastasiia ALOKHINA <aalokhin@e1r7p6.unit.ua>
2018-11-11 20:31:48 +0200

    a flag is something horrible

--- champs/ex.s
+++ champs/ex.s
@@ -1,7 +1,7 @@
 .name "zork"
 .comment "just a basic living prog"
 		
-l2:	sti	r1,%:live,%0  #aaaa
-	and	r1,%0,r1		#bbbb
-live:	live	%1		#cccc
-	zjmp	%:live		#dddd
+l2:	sti	r1,%:live,%0
+	and	r1,%0,r1
+live:	live	%1
+	zjmp	%:live
