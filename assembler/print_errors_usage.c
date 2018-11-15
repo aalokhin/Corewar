ebcdb67b5eb98bc647ebd33dd2bdd0b1b30a9e1c
Anastasiia ALOKHINA <aalokhin@e2r3p7.unit.ua>
2018-11-15 15:29:36 +0200

    music in assm

--- assembler/print_errors_usage.c
+++ assembler/print_errors_usage.c
@@ -1,12 +1,104 @@
 #include "asm.h"
 
+
+// #ifndef MUSIC_H
+// # define MUSIC_H
+
+// /*
+// ** Music structure
+// */
+
+// # define M                              v->music
+// # define MSTART                 1
+// # define MDIE                   2
+// # define MEND                   4
+
+// typedef struct                  s_single_music_info
+// {
+//         unsigned char           start_duration;
+//         unsigned char           die_duration;
+//         char                            *play_start;
+//         char                            *play_die;
+//         char                            *play_end;
+
+// }                                               t_smi;
+
+// struct                                  s_music
+// {
+//         unsigned char           music;
+//         unsigned char           to_wait;
+//         t_smi                           *cmi;
+
+//         time_t                          playing;
+// };
+
+// #endif
+
+
+// #define TMP                     t->play_start
+
+// static const t_smi      g_all_music_info[] = {
+//         {0, 0, NULL, NULL, NULL},
+//         {14, 14, "venom/", NULL, NULL},
+//         {8, 4, "skibidi/", NULL, NULL},
+//         {18, 12, "bigshaq/", NULL, NULL},
+//         {0, 0, NULL, NULL, NULL}
+// };
+
+// static bool                     init(t_vm *v)
+// {
+//         t_smi                   *const t = (t_smi *const)(g_all_music_info + M.music);
+
+//         TMP = ft_strjoin(
+//         "afplay /Users/vbrazas/projects/archive/corewar/vm/music/"
+//         , TMP);
+//         t->play_die = ft_strjoin(TMP, "die.mp3 &> /dev/null &");
+//         t->play_end = ft_strjoin(TMP, "end.mp3 &> /dev/null &");
+//         t->play_start = ft_strjoin(TMP, "start.mp3 &> /dev/null &");
+//         M.cmi = (t_smi*)t;
+//         return (true);
+// }
+
+// void                            play_music(t_vm *v, int flag)
+// {
+//         static int              is_allocate = 0;
+
+
+//         if (!A.is_ncurses || !M.music)
+//                 return ;
+//         if (is_allocate == 0)
+//                 init(v) && ++is_allocate;
+//         if (flag == MSTART)
+//         {
+//                 system(M.cmi->play_start);
+//                 M.to_wait = M.cmi->start_duration;
+//                 M.playing = time(NULL);
+//         }
+//         else if (flag == MDIE
+//         && M.playing + M.to_wait < time(NULL))
+//         {
+//                 system(M.cmi->play_die);
+//                 M.to_wait = M.cmi->die_duration;
+//                 M.playing = time(NULL);
+//         }
+//         else if (flag == MEND)
+//         {
+//                 system("pkill afplay");
+//                 system(M.cmi->play_end);
+//         }
+// }
+
+
 void 			ft_print_success(t_binfile *bin)
 {
 	int 		len;
 
-
+	
 	len = ft_strlen(bin->arg_name);
 	printf("Writing output program to %s\n", bin->res_arg_name);
+	system("afplay /Users/aalokhin/Downloads/Sound_15025.mp3");
+
+
 
 }
 
