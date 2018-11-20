0ba4b116d06192502b4a09912cd3228d44e81446
Anastasiia ALOKHINA <aalokhin@e2r3p7.unit.ua>
2018-11-20 15:29:58 +0200

    freeing funcs

--- assembler/clean_all.c
+++ assembler/clean_all.c
@@ -1,12 +1,56 @@
 #include "asm.h"
 
+void		clean_instructs(t_lable *element)
+{
+	t_t		*instruct;
+	t_t 	*tmp;
+
+	instruct = element->instruct;
+	while(instruct)
+	{
+		tmp = instruct;
+		ft_strdel(&(tmp->name_c));
+		ft_strdel(&(tmp->line_copy));
+
+		instruct = instruct->next;
+		free(tmp);
+	}
+	free(element);
+
+
+}
+
+void			clean_labels(t_lable **head)
+{
+	t_lable		*next;
+
+	next = *head;
+	if (!head)
+		return ;
+	while(*head != NULL)
+	{
+		next = (*head)->next;
+		ft_strdel(&(*head)->label_name);
+		ft_strdel(&(*head)->line_copy);
+		clean_instructs(*head);
+		(*head) = next;
+	}
+}
+
 void		ft_clean_all(t_binfile	*bin)
 {
+	// if (bin->labels_list)
+	// 	printf("1it exists\n");
 	ft_strdel(&((*bin).f_contents));
 	ft_strdel(&((*bin).copy)); 
 	ft_strdel(&((*bin).arg_name)); 
 	ft_strdel(&((*bin).res_arg_name)); 
 	ft_strdel(&((*bin).name)); 
-	ft_strdel(&((*bin).comment)); 
+	ft_strdel(&((*bin).comment));
+	clean_labels(&((*bin).labels_list));
+	// if (bin->labels_list)
+	// 	printf("2it exists\n");
+	// else
+	// 	printf("it is freed\n");
 
 }
